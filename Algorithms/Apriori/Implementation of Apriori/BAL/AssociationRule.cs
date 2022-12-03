c7273a148df764fc93776aae4d1edb0a7ca77fac
Abdulrhman Gamal <60278530+Abdo-Gamal@users.noreply.github.com>
2022-12-03 22:46:02 +0200

    5 algo

---
+++ Algorithms/Apriori/Implementation of Apriori/BAL/AssociationRule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL
+{
+    public class AssociationRule
+    {
+        public string Label { get; set; }// item 
+        public double Confidance { get; set; } //like A==>B
+        public double Support { get; set; }      // like A==>D but not same rule of confedanc
+    }
+}
---
+++ Algorithms/Apriori/Implementation of Apriori/BAL/ItemSet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL
+{
+    public class ItemSet : Dictionary<List<string>, int>
+    {
+        public string Label { get; set; }//item
+        public int Support { get; set; }// frquance
+    }
+}
